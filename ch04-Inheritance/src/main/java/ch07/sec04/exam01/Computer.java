1bcbf84d8c0d17b09a7aa6d6cb24dbe8b25c386a
smilevictory <leevictory7272@gmail.com>
2025-04-22 17:44:58 +0900

    0422 상속 기본문제 풀이(완료)

--- ch04-Inheritance/src/main/java/ch07/sec04/exam01/Computer.java
+++ ch04-Inheritance/src/main/java/ch07/sec04/exam01/Computer.java
@@ -2,5 +2,8 @@ package ch07.sec04.exam01;
 
 public class Computer extends Calculator{
     @Override
-    public double
+    public double areaCircle(double r) {
+        System.out.println("Calculator 객체의 areaCircle() 실행");
+        return Math.PI * r * r;
+    }
 }
